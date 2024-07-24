35ed06c48a3a1305664c65698a3db060e7698a78
Y0S0NG <yosongyxp@gmail.com>
2024-07-24 13:06:20 +0800

    添加了welcome页面后方的family poster页面

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,19 +1,14 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'; // 引入Bootstrap CSS
+import styles from './Result.module.css';
+import gifAnimation from './animation.gif'; // 引用 GIF 文件
 
-function Result() {
-  return (
-    <div className="p-5 mb-4 bg-light rounded-3">
-      <div className="container-fluid py-5">
-        <h1 className="display-5 fw-bold">提交成功！</h1>
-        <div className="row">
-          <div className="col">
-            <p className="fs-4">Family的分配结果将于__月__日通过公众号发布，请耐性等待！</p>
-          </div>
+function GifDisplay() {
+    return (
+        <div className={styles.centeredContainer}>
+            <img src={gifAnimation} className={styles.gif} alt="GIF Animation" />
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
-export default Result;
+export default GifDisplay;
+
